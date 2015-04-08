4b61b96148f7241e7de26c7e1fe27cbc8ba4d18d
jpuente.ext <jpuente.ext@inf-jpuente.riojasalud.es>
2015-04-08 20:55:16 +0200

    Mejorada opciones de enviar documento x email a varios destinatarios simultaneamente.

--- src/anjelica/gnu/chu/mail/IFMail.java
+++ src/anjelica/gnu/chu/mail/IFMail.java
@@ -48,7 +48,7 @@ public class IFMail extends ventana {
 
     public IFMail() {
         initComponents();
-        setSize(new Dimension(580,350));
+        setSize(new Dimension(500,350));
     }
 
     /** This method is called from within the constructor to
@@ -60,6 +60,8 @@ public class IFMail extends ventana {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        cTextArea1 = new gnu.chu.controles.CTextArea();
         cli_codiE = new gnu.chu.camposdb.cliPanel();
         cLabel1 = new gnu.chu.controles.CLabel();
         cLabel2 = new gnu.chu.controles.CLabel();
@@ -72,18 +74,35 @@ public class IFMail extends ventana {
         asuntoE = new gnu.chu.controles.CTextField(Types.CHAR,"X",100);
         cli_emailE = new gnu.chu.controles.CLinkBox();
         opCopia = new gnu.chu.controles.CCheckBox();
+        cLabel5 = new gnu.chu.controles.CLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        toEmailE = new gnu.chu.controles.CTextArea();
+        BIncluir = new gnu.chu.controles.CButton();
+
+        cTextArea1.setColumns(20);
+        cTextArea1.setRows(5);
+        jScrollPane1.setViewportView(cTextArea1);
 
         setTitle("Enviar Correo Electronico");
+        getContentPane().setLayout(null);
+        getContentPane().add(cli_codiE);
+        cli_codiE.setBounds(53, 11, 421, 18);
 
         cLabel1.setText("Cliente");
+        getContentPane().add(cLabel1);
+        cLabel1.setBounds(10, 14, 39, 15);
 
         cLabel2.setText("Correo");
+        getContentPane().add(cLabel2);
+        cLabel2.setBounds(10, 35, 39, 15);
 
         cLabel3.setBackground(java.awt.Color.blue);
         cLabel3.setForeground(new java.awt.Color(255, 255, 255));
         cLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         cLabel3.setText("Introduzca un texto a acompañar al Correo");
         cLabel3.setOpaque(true);
+        getContentPane().add(cLabel3);
+        cLabel3.setBounds(10, 130, 466, 15);
 
         scmsgE.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
@@ -92,81 +111,46 @@ public class IFMail extends ventana {
         respuestE.setRows(5);
         scmsgE.setViewportView(respuestE);
 
+        getContentPane().add(scmsgE);
+        scmsgE.setBounds(10, 148, 466, 90);
+
         Baceptar.setText("Aceptar");
+        getContentPane().add(Baceptar);
+        Baceptar.setBounds(140, 244, 156, 32);
 
         Bcancelar.setText("Cancelar");
+        getContentPane().add(Bcancelar);
+        Bcancelar.setBounds(304, 244, 156, 32);
 
         cLabel4.setText("Asunto");
+        getContentPane().add(cLabel4);
+        cLabel4.setBounds(10, 110, 40, 15);
+        getContentPane().add(asuntoE);
+        asuntoE.setBounds(60, 110, 410, 17);
 
         cli_emailE.setAncTexto(180);
+        getContentPane().add(cli_emailE);
+        cli_emailE.setBounds(53, 35, 330, 17);
 
         opCopia.setSelected(true);
         opCopia.setText("Copia Local");
+        getContentPane().add(opCopia);
+        opCopia.setBounds(20, 244, 90, 23);
 
-        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
-                .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(cLabel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(cLabel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(layout.createSequentialGroup()
-                        .add(cli_emailE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(opCopia, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                    .add(cli_codiE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 421, Short.MAX_VALUE))
-                .add(12, 12, 12))
-            .add(layout.createSequentialGroup()
-                .add(21, 21, 21)
-                .add(Baceptar, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 156, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .add(127, 127, 127)
-                .add(Bcancelar, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 156, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(26, Short.MAX_VALUE))
-            .add(layout.createSequentialGroup()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .add(scmsgE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 466, Short.MAX_VALUE))
-                    .add(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .add(cLabel3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 466, Short.MAX_VALUE))
-                    .add(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .add(cLabel4, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(asuntoE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 422, Short.MAX_VALUE)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
-                .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                    .add(cLabel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(cli_codiE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(cLabel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(cli_emailE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(opCopia, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .add(8, 8, 8)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(cLabel4, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(asuntoE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(cLabel3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(scmsgE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 127, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(Baceptar, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 32, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(Bcancelar, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 32, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(24, Short.MAX_VALUE))
-        );
+        cLabel5.setText("A");
+        getContentPane().add(cLabel5);
+        cLabel5.setBounds(10, 57, 20, 15);
+
+        toEmailE.setColumns(20);
+        toEmailE.setRows(5);
+        jScrollPane2.setViewportView(toEmailE);
+
+        getContentPane().add(jScrollPane2);
+        jScrollPane2.setBounds(30, 60, 440, 40);
+
+        BIncluir.setText("Incluir");
+        getContentPane().add(BIncluir);
+        BIncluir.setBounds(390, 35, 70, 19);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -193,6 +177,7 @@ public class IFMail extends ventana {
         {
             cli_codiE.setText(cliCodi);
             cli_emailE.removeAllItems();
+            toEmailE.setText("");
             llenaCorreos();
             cli_emailE.resetTexto();
         }
@@ -250,17 +235,32 @@ public class IFMail extends ventana {
                 cancelEmail();
             }
         });
+        
+        BIncluir.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (cli_emailE.isNull())
+                    return;
+                toEmailE.setText(toEmailE.getText()+
+                    (toEmailE.getText().trim().equals("")?"":", ")+cli_emailE.getText());
+            }
+        });
     }
     void enviarEmail()
     {
-      if (cli_emailE.isNull(true) )
+      if (toEmailE.getText().trim().equals(""))
+        toEmailE.setText(cli_emailE.getText());
+      
+      if (toEmailE.getText().trim().equals("") )
       {
-        msgBox("Introduzca Correo Electronico");
+      
+        msgBox("Introduzca algún Correo Electronico de destino");
         return;
       }
-        if (tipoDoc.equals("A"))
+      if (tipoDoc.equals("A"))
             enviarEmailAlb();
-        else
+      else
             enviarEmailFra();
     }
     void enviarEmailFra()
@@ -269,7 +269,7 @@ public class IFMail extends ventana {
             liFra.setSubject(asuntoE.getText());
             liFra.setEmailCC(opCopia.isSelected()? EU.email:null);
             liFra.setAsunto(respuestE.getText());
-            liFra.sendFraEmail(sqlDoc, cli_emailE.getText(), cli_codiE.getValorInt() );
+            liFra.sendFraEmail(sqlDoc, toEmailE.getText(), cli_codiE.getValorInt() );
             HashMap hm=new HashMap();
             hm.put("%c", cli_codiE.getValorInt());
             hm.put("%a",asuntoE.getText());
@@ -296,7 +296,7 @@ public class IFMail extends ventana {
             liAlb.setSubject(asuntoE.getText());
             liAlb.setEmailCC(opCopia.isSelected()? EU.email:null);
             liAlb.envAlbaranEmail(ct.getConnection(), dtStat, sqlDoc, EU,
-                            opValora,cli_emailE.getText(),
+                            opValora,toEmailE.getText(),
                             respuestE.getText());
             HashMap hm=new HashMap();
             hm.put("%c", cli_codiE.getValorInt());
@@ -322,6 +322,7 @@ public class IFMail extends ventana {
   }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private gnu.chu.controles.CButton BIncluir;
     private gnu.chu.controles.CButton Baceptar;
     private gnu.chu.controles.CButton Bcancelar;
     private gnu.chu.controles.CTextField asuntoE;
@@ -329,11 +330,16 @@ public class IFMail extends ventana {
     private gnu.chu.controles.CLabel cLabel2;
     private gnu.chu.controles.CLabel cLabel3;
     private gnu.chu.controles.CLabel cLabel4;
+    private gnu.chu.controles.CLabel cLabel5;
+    private gnu.chu.controles.CTextArea cTextArea1;
     private gnu.chu.camposdb.cliPanel cli_codiE;
     private gnu.chu.controles.CLinkBox cli_emailE;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private gnu.chu.controles.CCheckBox opCopia;
     private gnu.chu.controles.CTextArea respuestE;
     private javax.swing.JScrollPane scmsgE;
+    private gnu.chu.controles.CTextArea toEmailE;
     // End of variables declaration//GEN-END:variables
 
 }
