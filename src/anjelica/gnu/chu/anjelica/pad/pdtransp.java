9d1c0377bda3af5eef56e6d0911b520163b4f169
cpuente <cpuente@chuchip>
2017-06-20 21:23:46 +0200

    Limpieza codigo

--- src/anjelica/gnu/chu/anjelica/pad/pdtransp.java
+++ src/anjelica/gnu/chu/anjelica/pad/pdtransp.java
@@ -16,7 +16,7 @@ import javax.swing.BorderFactory;
  * <p>Título: PDTRANSP </p>
  * <p>Descripción: Mantenimiento Tabla de Transportistas</p>
  * <p>Empresa: MISL</p>
- * <p>Copyright: Copyright (c) 2005-2016
+ * <p>Copyright: Copyright (c) 2005-2017
  *  Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo
  *  los terminos de la Licencia Pública General de GNU según es publicada por
  *  la Free Software Foundation, bien de la versión 2 de dicha Licencia
